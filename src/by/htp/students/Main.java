3ae4f97bdb67a8011c5442b25f950520240f99fd
anna.khmialnitskaya <anna.khmialnitskaya@gmail.com>
2020-03-11 22:26:53 +0300

    Adding new student

--- src/by/htp/students/Main.java
+++ src/by/htp/students/Main.java
@@ -9,19 +9,20 @@ public class Main {
 		Student student1 = new Student("Dima", 30);
 		Student student2 = new Student("Vitaly", 31);
 		Student student3 = new Student("Olga", 32);
+		Student student4 = new Student("Ivan", 33);
 		Group group = admin.createGroup(5, 3);
 
 		admin.enrollStudent(student3, group);
 		admin.enrollStudent(student1, group);
 		admin.enrollStudent(student2, group);
+		admin.enrollStudent(student4, group);
 
 		admin.printGroup(group);
 		System.out.println("" + admin);
-		
-		
+
 		admin.sortStudents(group);
-		admin.printGroup(group);		
-		
+		admin.printGroup(group);
+
 		admin.deleteStudentWithMinRating(admin.minRating(group), group);
 		admin.printGroup(group);
 	}
